d8ce06b6d04ebe3fb03c50b76af91314c423dd61
Aqshal Fajar Putra <aqshalfajarputra0226@gmail.com>
2016-10-09 22:18:39 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -8,6 +8,24 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void composeSmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
+
+    public void dialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel : " + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +39,14 @@ public class MainActivity extends AppCompatActivity {
                                         }
                                     }
                 );
-    }
 
-    public void dialPhoneNumber(String phoneNumber) {
-        Intent intent = new Intent(Intent.ACTION_DIAL);
-        intent.setData(Uri.parse("tel : " + phoneNumber));
-        if (intent.resolveActivity(getPackageManager()) != null)
-            startActivity(intent);
+        findViewById(R.id.imageViewSMS)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        composeSmsMessage("Pesan Dari SMK Telkom Malang");
+                    }
+                });
     }
 
 }
